162a14dfb5d9197e5b34e16da5c76652fd2148e8
Ian Miell <ian.miell@gmail.com>
2017-07-15 15:11:31 +0100

    useless

--- grab.sh
+++ grab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 google-chrome --headless --hide-scrollbars --remote-debugging-port=9222 --disable-gpu &                                                                                                           
 # Take the screenshot                                                                                                                                                                                 
-nodejs index.js --url="http://www.eff.org"
+nodejs index.js --url="https://www.theuselesswebindex.com/website/"
 cp output.png /output.png
