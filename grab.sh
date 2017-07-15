f3390d72cecd0397f0b84caa94a8a196c1b04b18
Ian Miell <ian.miell@gmail.com>
2017-07-15 15:22:01 +0100

    latest

--- grab.sh
+++ grab.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-google-chrome --headless --hide-scrollbars --remote-debugging-port=9222 --disable-gpu &                                                                                                           
-# Take the screenshot                                                                                                                                                                                 
+google-chrome --headless --hide-scrollbars --remote-debugging-port=9222 --disable-gpu &
+# Take the screenshot
+echo 'Visiting: https://www.theuselesswebindex.com/website/'
 nodejs index.js --url="https://www.theuselesswebindex.com/website/"
 cp output.png /output.png
+echo 'Screenshot grabbed to output.png'
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 docker build -t grab .
 CID=$(docker run -d --privileged grab)
-sleep 5
+echo 'Waiting for grab to complete'
+sleep 10
+echo 'Copying output.png'
 docker cp ${CID}:/output.png .
 docker rm -f ${CID}
